34ffe551c94e52bc92665ab3f624c2267783af71
Matthew Jordan <mattistrees@gmail.com>
2025-02-02 19:35:59 -0500

    Add new views corresponding to the poll questions in models.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello World. You are at the main application index!")
+
+def detail(request, question_id):
+	return HttpResponse("You're looking for question %s." % question_id)
+
+def results(request, question_id):
+	return HttpResponse("You're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
